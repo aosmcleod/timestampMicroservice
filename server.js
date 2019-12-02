a3691608fe856a9a3180e80425e59005aad674e4
Glitch (fcc-timestamp-microservice-) <none>
2019-12-02 19:27:29 +0000

    🏝🎊 Updated with Glitch

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // where your node app starts
 
 // init project
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
@@ -24,9 +25,43 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  //console.log('Your app is listening on port ' + listener.address().port);
+});
+
+// timestamp
+app.get('/api/timestamp', function(req, res) {
+  var now = new Date();
+    res.json({
+      'unix': now.getTime(),
+      'utc': now.toUTCString()
+    });
+})
+
+
+app.get('/api/timestamp/:date_string', function(req, res) {
+  var startTime = new Date().toLocaleString();
+  console.group(startTime);
+  console.log('Initiating request...');
+  console.log('Requested date_string: ' + req.params.date_string);
+  var requestedDate = new Date(req.params.date_string);
+  if (requestedDate != 'Invalid Date') {
+    res.json({
+      'unix': requestedDate.getTime(),
+      'utc': requestedDate.toUTCString()
+    });
+  } else {
+    requestedDate = new Date(req.params.date_string/1000).toUTCString();
+    if (requestedDate != 'Invalid Date') {
+      res.json({
+        'unix': req.params.date_string,
+        'utc': requestedDate
+      });
+    } else {
+        res.json({'error': 'Invalid Date'});
+    }
+  }
+  console.log('Result: ' + res);
+  console.groupEnd(startTime);
 });
